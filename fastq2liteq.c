9000fa15410eae032053c379b758cf6e9f5fcae7
Charles <depalman17@gmail.com>
2019-02-26 13:40:58 -0500

    wow, I havent worked on this in a long time

--- fastq2liteq.c
+++ fastq2liteq.c
@@ -1,9 +1,21 @@
 #include "util.h"
 
+#define NEWLINE_STRING "\n"
+
+/* an intermediate form for a single fastq file entry. */
+struct interm_entry
+{
+	char * seqidline;
+	char * readsline;
+	char * plusline;
+	char * scoresline;
+};
+
+// TODO: sanity check for huge fastq files that would exceed memory capacity
 int main ( int argc, char * * argv )
 {
-	char * infilename = argv[1];
-	char * outfilename;
+//	char * infilename = argv[1];
+//	char * outfilename;
 
 	struct liteq_file out = 
 	{
@@ -14,41 +26,42 @@ int main ( int argc, char * * argv )
 	};
 
 	char * file = fromFile( infilename );
-	int entries = linesIn( file ) % 4;
+	
+	int entries = linesIn( file ) / 4; // truncate up to 3 trailing blank lines
 
-//	for ( char * c; ( c = strtok( file, "\n" ) ) != '\0'; )
-	char * c;
-	while ( 1 )
+	out.linecount = entries; // 
+
+	struct interm_entry * interms = malloc( entries * sizeof(struct interm_entry) );
+
+	for ( int i = 0; i < entries; i++ )
 	{
-		// First line, sequence ID
-		c = strtok( file, "\n" );
-		if ( *c != '@' ) err ( -1, "Malformed FASTQ file \"%s\"\n", infilename );
-
-		// Second line, reads
-		c = strtok( file, "\n" );
-		
-		// Third line, plus
-		c = strtok( file, "\n" );
-		if ( *c != '+' ) err ( -1, "Malformed FASTQ file \"%s\"\n", infilename );
-
-		// Fourth line, scores
-		c = strtok( file, "\n" );
-
-	}	
+		interms[i].seqidline  = strtok( file, NEWLINE_STRING );
+		interms[i].readsline  = strtok( file, NEWLINE_STRING );
+		interms[i].plusline   = strtok( file, NEWLINE_STRING );
+		interms[i].scoresline = strtok( file, NEWLINE_STRING );
+	}
 
 	free( file );
+
+	struct liteq_line * outlines = malloc( entries * sizeof(struct liteq_line) );	
+
+	for ( int i = 0; i < entries; i++ )
+	{
+		outlines[i].readcount = strlen( interms[i].readsline );
+	}
+
+	free( interms );
+	free( outlines );
 	return 0;
 }
 
 int linesIn( char * str )
 {
 	int lines = 0;
-
 	while ( *(str++) != '\0' )
 	{
 		if ( *str == '\n' ) lines++;
 	}
-
 	return lines;
 }
 
