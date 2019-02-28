7c79931a683ad393748f31a0cefe0fa63512e7c5
Charles <depalman17@gmail.com>
2019-02-28 16:37:52 -0500

    added actual manipulation of interms into binary

--- fastq2liteq.c
+++ fastq2liteq.c
@@ -22,6 +22,11 @@ int linesIn( char * str )
 	return lines;
 }
 
+void liteqDisplay( struct liteq_file disp )
+{
+	printf( "Magic Number: %x\n", 
+}
+
 // TODO: sanity check for huge fastq files that would exceed memory capacity
 int main ( int argc, char * * argv )
 {
@@ -63,16 +68,36 @@ int main ( int argc, char * * argv )
 			interms[i].scoresline
 		);
 	}
+	
 	free( file );
 
 	struct liteq_line * outlines = malloc( entries * sizeof(struct liteq_line) );	
+	out.lines = outlines;
 
 	for ( int i = 0; i < entries; i++ )
 	{
 		outlines[i].readcount = strlen( interms[i].readsline );
-		
+
+		/* TODO: make a single call to malloc instead of iterating */
+		outlines[i].reads = malloc( outlines[i].readcount * sizeof(uint8_t) );
+
+		for ( int j = 0; j < outlines[i].readcount; j++ )
+		{
+			outlines[i].reads[j] = packread
+			(
+				char2Base(  interms[i].readsline[j]  ),
+				char2Score( interms[i].scoresline[j], SANGER_ASCII_OFFSET )
+			);
+		}
+
 	}
 
+	for ( int i = 0; i < entries; i++ )
+	{
+	}
+	
+	
+	for ( int i = 0; i < entries; i++ ) free( outlines[i].reads );
 	free( interms );
 	free( outlines );
 	return 0;
